488dcef665dd1eace5f3fd1179f5ec4ed76e4d09
fabi <fpavez@alumnes.aulabalear.org>
2023-10-27 19:32:18 +0200

    asdf

---
+++ prueba.py
@@ -0,0 +1,31 @@
+#def metodo1():
+    
+#    print ("¡Hola Mundo!")
+
+#def metodo2():
+    
+#    print ("¿Como estás hoy?")
+
+#def resta(a,b):
+#    return a - b
+#def multiplicacion(a,b):
+#    return a * b
+#def despedida():
+#    print(Adios)
+def print_array(array):
+    for a in array:
+        print(a)
+
+if __name__ == "__main__":
+
+    #metodo1()
+    #metodo2()
+    #resta(10, 20)
+    #a = resta(5,4)
+    #b = multiplicacion(5, 4)
+    #print (a)
+    #print (b)
+    #Adios = "¡Hasta luego!"
+    #despedida()
+    array = ["Manolo", "Antonio", "Victor"]
+    print_array(array)
