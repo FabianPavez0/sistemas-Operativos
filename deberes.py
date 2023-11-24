e91470e8de229ab35ef1d5ec74ae4e262f3cd16f
fabi <fpavez@alumnes.aulabalear.org>
2023-11-24 19:57:43 +0100

    avance

---
+++ deberes.py
@@ -0,0 +1,12 @@
+# Def busqueda_while (arr):
+
+def arr():
+   arr = ["macia", "pepe", "juan"] 
+
+i=0
+encontrado = False
+while not encontrado:
+    if "pepe" == arr[i]:
+        print ("Encontrado")
+        encontrado = True
+    i+=1
