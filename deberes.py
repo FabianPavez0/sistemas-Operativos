8e1d3ed36e44fe02057c8cac0b02f1c5447d1e91
fabi <fpavez@alumnes.aulabalear.org>
2023-11-27 15:34:25 +0100

    deberes

--- deberes.py
+++ deberes.py
@@ -1,12 +1,39 @@
+'''
 # Def busqueda_while (arr):
 
-def arr():
-   arr = ["macia", "pepe", "juan"] 
+def array_nombres():
+    arr = ["macia", "jose", "juan", "ernes"]
+    return arr
+
+#Main
 
 i=0
-encontrado = False
-while not encontrado:
-    if "pepe" == arr[i]:
-        print ("Encontrado")
-        encontrado = True
+var_a_encontrado = False
+array=array_nombres()
+while var_a_encontrado == False:
+    if array[i] == "juan":
+        var_a_encontrado = True
+        print(array[i])
     i+=1
+'''
+
+# Def busquedar_for (arr, var_encontrar)
+
+def array_nombres():
+    arr = ["macia", "jose", "juan", "ernes"]
+    return arr
+
+#Main
+
+i=0
+var_encontrar = False
+array=array_nombres()
+
+for nombre_lista in array:
+    if "juan" in nombre_lista:
+        var_encontrar = True
+        print(nombre_lista)
+i+=1
+
+
+# Def recorrido_while (arr):
